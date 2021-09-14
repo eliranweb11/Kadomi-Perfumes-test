f31efc69f088d1f8612c753312f829cdc77a1a54
eliranweb11 <eliran.ambar@gmail.com>
2021-09-14 16:59:43 +0300

    finish all

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
         $(this).next('.content').slideToggle(200);
     });
 
+    
     //questions
     $('.answer-item').click(function () {
         var question = $(this).parents('.question-item').attr('data-question');
@@ -22,11 +23,40 @@ $(document).ready(function(){
         goToQuestion(nextQuestion);
     });
 
+    $('#footer-menu .menu-item a').click(function () {
+        var question = $(this).attr('data-question');
+        goToQuestion(question);
+    });
+
     function goToQuestion(question) {
         $('.question-item').hide();
         $('.question-item[data-question="' + question + '"]').show();
     }
 
+    //menu - slideout
+    $('.slideout-menu-wrp ul li a').click(function () {
+        var question = $(this).parents('.menu-item').attr('data-category');
+        var answer = $(this).attr('data-answer');
+        console.log('question = ' + question + ' answer = ' + answer);
+
+        var nextQuestion = parseInt(question) + 1;
+        goToQuestion(nextQuestion);
+    });
+    $('.slideout-menu-wrp ul li a').click(function () {
+        var question = $(this).attr('data-category');
+        goToQuestion(question);
+        $('.slideout-menu ul.slideout-menu-category').hide();
+    });
+    //arrow back
+    $('.menu-item-icon_back').click(function () {
+        $('.slideout-menu ul.slideout-menu-category').show();
+        $('.menu-item').hide();
+        
+    });
+    function goToQuestion(question) {
+        $('.menu-item').hide();
+        $('.menu-item[data-category="' + question + '"]').show();
+    }
 
 });
 
@@ -43,11 +73,13 @@ document.addEventListener('DOMContentLoaded', function() {
     toggleButton.addEventListener('click', function(e) {
         e.preventDefault();
         menu.classList.toggle('is-open');
+        toggleButton.classList.toggle('btn-open');
     });
 
     // Close Menu
     closeButton.addEventListener('click', function(e) {
         e.preventDefault();
         menu.classList.remove('is-open');
+        toggleButton.classList.remove('btn-open');
     });
 });
